0aaacfa76ca1e232340fff79d92c53cdae80e8d7
Gábor Náray <drszamo@gmail.com>
2023-03-17 10:50:57 +0100

    Command line builder files for Borland C++ 6, compiling the files named in English.
    The BCC switches are not fine tuned so the size of built exe differs from the size of original exe.
    Size of original exe: 794624 bytes
    Size of built exe: 116224 bytes
    Size of built exe by Borland C++ 6 GUI: 69120 bytes

---
+++ Source/BCBconfigB6.bat
@@ -0,0 +1,5 @@
+SET BCB=c:\Program Files\Borland\CBuilder6
+SET BCC_OPTIONS=-O2 -Hc -Q -Vx -Ve -X- -a8 -6 -b- -k- -vi -tW -tWM- -D;USEPACKAGES
+@rem -WV : Target uses the VCL GUI framework
+@rem Not using following options because linking separately: -lD"" -lap -lTpe -lGn
+@rem -lx : Linker: No map
