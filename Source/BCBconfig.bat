afba3615716904deb2cb1edf63de8fdc4dfa5be9
Gábor Náray <drszamo@gmail.com>
2023-03-17 10:48:50 +0100

    Command line builder files for Borland 4 (alias Turbo Explorer 2006), compiling the files named in French.
    The BCC switches are not fine tuned so the size of built exe differs from the size of original exe.
    Size of original exe: 794624 bytes
    Size of built exe: around 69000 bytes

---
+++ Source/BCBconfig.bat
@@ -0,0 +1,5 @@
+SET BCB=c:\Program Files\Borland\BDS\4.0
+SET BCC_OPTIONS=-O2 -Hc -Q -Vx -Ve -X- -a8 -6 -b- -k- -vi -tW -tWM- -D;USEPACKAGES
+@rem -WV : Target uses the VCL GUI framework
+@rem Not using following options because linking separately: -lD"" -lap -lTpe -lGn
+@rem -lx : Linker: No map
---
+++ Source/compile.bat
@@ -0,0 +1,6 @@
+call BCBconfig.bat
+"%BCB%\bin\bcc32.exe" %BCC_OPTIONS% -c -I"%BCB%\Include";"%BCB%\Include\vcl" -L"%BCB%\lib" ST_Recover.cpp Unit1.cpp Acces_disque.cpp Analyse_disque.cpp Classe_Disquette.cpp Classe_Piste.cpp vcl.bpi
+"%BCB%\bin\ilink32.exe" @ST_Recover.rsp
+@rem https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Compiling_a_C%2B%2B_Application_from_the_Command_Line
+@rem https://docwiki.embarcadero.com/RADStudio/Sydney/en/BCC32.EXE,_the_C%2B%2B_32-bit_Command-Line_Compiler
+@rem https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Using_ILINK32_and_ILINK64_on_the_Command_Line
