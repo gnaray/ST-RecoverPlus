e21988d856c0081efe9083cc39c4762dc48817c9
Gábor Náray <drszamo@gmail.com>
2024-06-30 18:16:31 +0200

    Renamed Unit1.cpp as GUIForm1.cpp.

--- Source/compile.ST_Recover.bat
+++ Source/compile.ST_Recover.bat
@@ -1,5 +1,5 @@
 call BCBconfig.bat
-"%BCB%\bin\bcc32.exe" %BCC_OPTIONS% -c -I"%BCB%\Include";"%BCB%\Include\vcl" -L"%BCB%\lib" ST_Recover.cpp Unit1.cpp Acces_disque.cpp Analyse_disque.cpp Classe_Disquette.cpp Classe_Piste.cpp vcl.bpi
+"%BCB%\bin\bcc32.exe" %BCC_OPTIONS% -c -I"%BCB%\Include";"%BCB%\Include\vcl" -L"%BCB%\lib" ST_Recover.cpp GUIForm1.cpp Acces_disque.cpp Analyse_disque.cpp Classe_Disquette.cpp Classe_Piste.cpp vcl.bpi
 "%BCB%\bin\ilink32.exe" @ST_Recover.rsp
 @rem https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Compiling_a_C%2B%2B_Application_from_the_Command_Line
 @rem https://docwiki.embarcadero.com/RADStudio/Sydney/en/BCC32.EXE,_the_C%2B%2B_32-bit_Command-Line_Compiler
